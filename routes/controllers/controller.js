f8c4d99e319b4f6ede8796d204d38f2dfd05b26f
Alander Sanna <sanna.alander@aalto.fi>
2021-02-27 16:16:41 +0200

    some changes

--- routes/controllers/controller.js
+++ routes/controllers/controller.js
@@ -1,13 +1,36 @@
 
 const manufacturers = [];
-const gloves = await fetch('https://bad-api-assignment.reaktor.com/v2/products/gloves');
-const gloves_data = await gloves.json();
-const masks = await fetch('https://bad-api-assignment.reaktor.com/v2/products/facemasks');
-const masks_data = await masks.json();
-const beanies = await fetch('https://bad-api-assignment.reaktor.com/v2/products/beanies');
-const beanies_data = await beanies.json();
 
-const all_data = gloves_data.concat(masks_data, beanies_data);
+const getData = async(type) => {
+    const api_url = 'https://bad-api-assignment.reaktor.com/v2/products/'+type;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    return data;
+}
+
+const gloves = new Promise(
+    (resolve) => resolve(getData('gloves'))
+);
+const masks = new Promise(
+    (resolve) => resolve(getData('facemasks'))
+);
+const beanies = new Promise(
+    (resolve) => resolve(getData('beanies'))
+);
+
+const responses = await Promise.all([gloves, masks, beanies]);
+let all_data = [];
+let gloves_data = [];
+let masks_data = [];
+let beanies_data = [];
+
+for (const res of responses) {
+    all_data = all_data.concat(res);
+    if (res[0].type == 'gloves') {gloves_data = res}
+    else if (res[0].type == 'facemasks') {masks_data = res}
+    else if (res[0].type == 'beanies') {beanies_data = res}
+}
+
 for (const i of all_data) {
     if (!manufacturers.includes(i.manufacturer)) {
         manufacturers.push(i.manufacturer);
@@ -19,19 +42,17 @@ const availabilities = {};
 const getAvailability = async(brand) => {
     const api_url = 'https://bad-api-assignment.reaktor.com/v2/availability/'+brand;
     const response1 = await fetch(api_url);
-    //if (response1) {
-        const data = await response1.json();
+    const data = await response1.json();
 
-        availabilities[brand] = {};
-        if (data.response.length != 0) {
-            for (const j of data.response) {
-                if (j.DATAPAYLOAD) {
-                    availabilities[brand][j.id] = j.DATAPAYLOAD.substring(50, j.DATAPAYLOAD.length-31);
-                }
+    availabilities[brand] = {};
+    if (data.response.length > 2) {
+        console.log(data.response.length);
+        for (const j of data.response) {
+            if (j.DATAPAYLOAD) {
+                availabilities[brand][j.id] = j.DATAPAYLOAD.substring(50, j.DATAPAYLOAD.length-31);
             }
         }
-        
-    //}   
+    }   
 }
 const promises = [];
 
@@ -44,7 +65,7 @@ for (const m in manufacturers) {
 await Promise.all(promises);
 
 
-const getItems = async(data) => {
+const addAvailability = async(data) => {
 
     for (const item of data) {
         const m_id = item.id.toUpperCase();
@@ -53,18 +74,16 @@ const getItems = async(data) => {
         } else {
             item["availability"] = 'UNKNOWN'; 
         }
-        
 
-        console.log(item.availability);
     }
 
     return data;
 }
 
 const data = {
-    gloves: await getItems(gloves_data),
-    masks: await getItems(masks_data),
-    beanies: await getItems(beanies_data)
+    gloves: await addAvailability(gloves_data),
+    masks: await addAvailability(masks_data),
+    beanies: await addAvailability(beanies_data)
 };
 
 const showGloves = async({render}) => {
